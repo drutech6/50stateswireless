21916d9f7d9a827392054d1c653efc87ee108e6b
drutech6 <linux.vg@gmail.com>
2025-12-29 11:57:02 -0500

    Delete _site directory

--- _site/stop-50states.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Use the command you verified works, adding 'ruby' just to be safe 
-# and the -9 flag to force an immediate stop.
-pkill -9 -f "jekyll|decap-server|ruby"
-
-# We don't need a sleep or echo here because we want it to 
-# execute the command and finish instantly.
-exit 0
